1dcb20fa4d5367861385e53f60c5b29c617695e7
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-05-12 10:01:03 +0530

    Added chunk, which give back list into chunks with given chunkSize and overlapSize

--- src/calender.js
+++ src/calender.js
@@ -8,4 +8,14 @@ const createSeries = function(start, end) {
   return series;
 }
 
+const chunk = function(list, size, overlap) {
+  if(list.length === 0) return list;
+  if(list.length <= overlap) return [list];
+
+  const currentChunk = list.slice(0, size);
+  const remaining = list.slice(size - overlap);
+  return [currentChunk].concat(chunk(remaining, size, overlap));
+}
+
 exports.createSeries = createSeries;
+exports.chunk = chunk;
--- test/calender-test.js
+++ test/calender-test.js
@@ -1,6 +1,6 @@
 const { describe, it } = require('node:test');
-const { deepStrictEqual } = require('assert');
-const { createSeries } = require('../src/calender.js');
+const { deepStrictEqual, strictEqual } = require('assert');
+const { createSeries, chunk } = require('../src/calender.js');
 
 describe('createSeries', function () {
   it('should give list of series of increment of one', function () {
@@ -8,4 +8,24 @@ describe('createSeries', function () {
     const expected = [1, 2, 3, 4, 5];
     deepStrictEqual(actual, expected);
   });
+
+  it('should give chunks of 2 with overlap size 0', function() {
+    const numbers = [1,2,3,4,5];
+    const chunkSize = 2;
+    const overlapSize = 0;
+    const actual = chunk(numbers, chunkSize, overlapSize);
+    const expected = [[1,2], [3,4], [5]];
+
+    deepStrictEqual(actual, expected);
+  });
+
+  it('should give chunks of 2 with overlap size 1', function() {
+    const numbers = [1,2,3,4,5];
+    const chunkSize = 2;
+    const overlapSize = 1;
+    const actual = chunk(numbers, chunkSize, overlapSize);
+    const expected = [[1,2], [2,3], [3,4], [4,5], [5]];
+
+    deepStrictEqual(actual, expected);
+  })
 });
