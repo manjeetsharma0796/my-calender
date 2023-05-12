d3f523597cc5c57589e1cbe99baa220a2ef3c8d0
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-05-12 10:11:09 +0530

    Moved chunk function to lib directory, and it is functional

--- test/calender-test.js
+++ test/calender-test.js
@@ -9,23 +9,5 @@ describe('createSeries', function () {
     deepStrictEqual(actual, expected);
   });
 
-  it('should give chunks of 2 with overlap size 0', function() {
-    const numbers = [1,2,3,4,5];
-    const chunkSize = 2;
-    const overlapSize = 0;
-    const actual = chunk(numbers, chunkSize, overlapSize);
-    const expected = [[1,2], [3,4], [5]];
-
-    deepStrictEqual(actual, expected);
-  });
-
-  it('should give chunks of 2 with overlap size 1', function() {
-    const numbers = [1,2,3,4,5];
-    const chunkSize = 2;
-    const overlapSize = 1;
-    const actual = chunk(numbers, chunkSize, overlapSize);
-    const expected = [[1,2], [2,3], [3,4], [4,5], [5]];
-
-    deepStrictEqual(actual, expected);
-  })
+  
 });
